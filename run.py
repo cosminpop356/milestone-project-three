b5eca93fdc5d460e871fa782a661d3b230b0f5bb
cosminpop356 <cosminpop356@gmail.com>
2022-11-13 15:06:05 +0000

    Create class and init function

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
 from random import randint
+
+class Board:
+    """
+    Creates an instance of Board
+    """
+    
+    def __init__(self, size, name, num_ships, type):
+        self.size = size
+        self.name = name
+        self.num_ships = num_ships
+        self.type = type
+        self.board = [["[]" for x in range(size)] for y in range(size)]
+
+
